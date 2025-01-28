8c5b166df16264ae0e81b9400dd889f7decd5ab8
gentlementlegen <fernand.veyrier@epitech.eu>
2025-01-28 19:23:18 +0900

    chore: removed log

--- static/scripts/onboarding/onboarding.ts
+++ static/scripts/onboarding/onboarding.ts
@@ -447,7 +447,6 @@ async function populateOrgs() {
     const pluginKit = Octokit.plugin(createOrUpdateTextFile);
     const octokit = new pluginKit({ auth: getSessionToken() });
     const { data } = await octokit.rest.orgs.listForAuthenticatedUser({ per_page: 100 });
-    console.log(data);
     const selectContainer = document.getElementById("orgName");
     if (selectContainer) {
       selectContainer.innerHTML = "";
